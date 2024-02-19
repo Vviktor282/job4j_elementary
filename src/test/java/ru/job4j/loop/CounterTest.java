09ddb0bb0dbaeb7a40db5dfb45792890fa288669
Vviktor282 <jnnvyy3i@gmail.com>
2024-02-19 22:04:37 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CounterTest {
 
     @Test
-    void whenStart1Finish5Then15() {
+    void whenSumFrom1To5Then15() {
         int start = 1;
         int finish = 5;
         int result = Counter.sum(start, finish);
@@ -18,7 +18,7 @@ class CounterTest {
     }
 
     @Test
-    void whenStart20Finish8Then0() {
+    void whenSumFrom20To0Then0() {
         int start = 20;
         int finish = 0;
         int result = Counter.sum(start, finish);
@@ -27,7 +27,7 @@ class CounterTest {
     }
 
     @Test
-    void whenStart1Finish1Then1() {
+    void whenSumFrom1To1Then1() {
         int start = 1;
         int finish = 1;
         int result = Counter.sum(start, finish);
@@ -36,7 +36,7 @@ class CounterTest {
     }
 
     @Test
-    void whenStart0Finish10Then1() {
+    void whenSumFrom0To10Then55() {
         int start = 0;
         int finish = 10;
         int result = Counter.sum(start, finish);
@@ -45,11 +45,38 @@ class CounterTest {
     }
 
     @Test
-    void whenStart3Finish8Then33() {
+    void whenSumFrom3To8Then33() {
         int start = 3;
         int finish = 8;
         int result = Counter.sum(start, finish);
         int expected = 33;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromMinusTwoToTenThenThirty() {
+        int start = -2;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 28;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromMinusTenToMinusOneThenThirty() {
+        int start = -10;
+        int finish = -1;
+        int result = Counter.sumByEven(start, finish);
+        int expected = -30;
         assertThat(result).isEqualTo(expected);
     }
 }
